1398f4c1ba0f2d625de6d7d029d6f76c6634c458
Rob <tehrhawb@gmail.com>
2017-07-13 18:18:39 -0400

    ???

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Action<string> sayGreeting;
+
+			Func<string, string> conversate = (message) => 
+			{
+				Console.WriteLine(message);
+				return Console.ReadLine();
+			};
+
+			string input = conversate("What's your name?");
+
+			sayGreeting = (greeting) =>
+			{
+				Console.WriteLine(string.Format(greeting, input));
+			};
+
+			sayGreeting("Hello {0}");
+			conversate("Good to see you.");
+			conversate("Y u do dis?");
+			sayGreeting("Later {0}");
+
+			Console.Read();
+        }
+    }
+}
